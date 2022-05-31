77c266e184fcc3cf610139624b84480c15b304c8
Werefox <jack@wrightus.com>
2022-05-31 14:08:18 -0400

    Made basic express server

---
+++ controllers/books_controller.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const book = express.Router()
+
+module.exports = book
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// dotenv
+require('dotenv').config()
+const PORT = process.env.PORT
+
+// mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+	() => { console.log('Connected to mongo: ', process.env.MONGO_URI) }	
+)
+
+
+
+// routes
+// landing page
+app.get('/', (req, res) => {
+	res.send("Welcome to Books-API")
+})
+
+// controllers
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
+
+// 404
+app.get('*', (req, res) => {
+	res.status(404).send('Error: 404')
+})
+
+
+
+// listen
+app.listen(PORT, () => {
+	console.log('Server listening at port ', PORT)
+})
