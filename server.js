832db8b5e92875b626cd1c7a94b4b01613e56e60
Werefox <jack@wrightus.com>
2022-06-02 16:05:32 -0400

    Renamed the mongo uri variable to 'MONGODB_URI'

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 // mongoose
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
-	() => { console.log('Connected to mongo: ', process.env.MONGO_URI) }	
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+	() => { console.log('Connected to mongo: ', process.env.MONGODB_URI) }	
 )
 
 
